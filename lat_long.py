a7476fa6fdfabc346d0e54636e45581647d47222
MetalDent <priyapandya274@gmail.com>
2019-05-28 13:58:51 +0530

    Add speed plt file

--- lat_long.py
+++ lat_long.py
@@ -6,24 +6,28 @@ import numpy as np
 birddata = pa.read_csv("bird_tracking.csv") 
 bird_names = pa.unique(birddata.bird_name)  
 
+# For Nico
 ix = birddata.bird_name == "Nico" 
 x,y = birddata.longitude[ix], birddata.latitude[ix] 
 plt.figure(figsize = (7,7)) 
 plt.title("Nico")
 plt.plot(x,y,"b.") 
 
+# For Eric 
 ix = birddata.bird_name == "Eric" 
 x,y = birddata.longitude[ix], birddata.latitude[ix] 
 plt.figure(figsize = (7,7)) 
 plt.title("Eric")
 plt.plot(x,y,"b.") 
 
+# For Sanne
 ix = birddata.bird_name == "Sanne" 
 x,y = birddata.longitude[ix], birddata.latitude[ix] 
 plt.figure(figsize = (7,7)) 
 plt.title("Sanne")
 plt.plot(x,y,"b.") 
   
+# Trajectory for all three
 plt.figure(figsize = (7,7)) 
 for bird_name in bird_names: 
     ix = birddata.bird_name == bird_name   
---
+++ speed_freq.py
@@ -0,0 +1,41 @@
+import pandas as pd 
+import matplotlib.pyplot as plt 
+import numpy as np 
+  
+birddata = pd.read_csv("bird_tracking.csv") 
+bird_names = pd.unique(birddata.bird_name)  
+
+# For Eric 
+ix = birddata.bird_name == "Eric" 
+speed = birddata.speed_2d[ix] 
+  
+plt.figure(figsize = (8,4)) 
+ind = np.isnan(speed) 
+plt.hist(speed[~ind], bins = np.linspace(0,30,20), normed=True) 
+plt.xlabel(" 2D speed (m/s) ") 
+plt.ylabel(" Frequency ") 
+plt.title("Eric")
+
+# For Sanne
+ix = birddata.bird_name == "Sanne" 
+speed = birddata.speed_2d[ix] 
+  
+plt.figure(figsize = (8,4)) 
+ind = np.isnan(speed) 
+plt.hist(speed[~ind], bins = np.linspace(0,30,20), normed=True) 
+plt.xlabel(" 2D speed (m/s) ") 
+plt.ylabel(" Frequency ") 
+plt.title("Sanne")
+
+# For Nico
+ix = birddata.bird_name == "Nico" 
+speed = birddata.speed_2d[ix] 
+  
+plt.figure(figsize = (8,4)) 
+ind = np.isnan(speed) 
+plt.hist(speed[~ind], bins = np.linspace(0,30,20), normed=True) 
+plt.xlabel(" 2D speed (m/s) ") 
+plt.ylabel(" Frequency ") 
+plt.title("Nico")
+
+plt.show() 
